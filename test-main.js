269272d15ec449e044010f17b5051a0c27059f79
Christopher Schleiden <cschleid@microsoft.com>
2016-06-30 16:00:50 -0700

    Fix test-main

--- test-main.js
+++ test-main.js
@@ -1,5 +1,5 @@
-var allTestFiles = []
-var TEST_REGEXP = /(spec|tests)\.js$/i
+var allTestFiles = [];
+var TEST_REGEXP = /(spec|tests)\.js$/i;
 
 // Get a list of all the test files to include
 Object.keys(window.__karma__.files).forEach(function (file) {
@@ -7,8 +7,8 @@ Object.keys(window.__karma__.files).forEach(function (file) {
     // Normalize paths to RequireJS module names.
     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
     // then do not normalize the paths
-    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '')
-    allTestFiles.push(normalizedTestModule)
+    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '');
+    allTestFiles.push(normalizedTestModule);
   }
 })
 
@@ -23,6 +23,5 @@ require.config({
   // dynamically load all test files
   deps: allTestFiles,
 
-  // we have to kickoff jasmine, as it is asynchronous
   callback: window.__karma__.start
-})
+});
